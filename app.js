0e3a16fe58c5d875c9a2db87129449942cfff3c0
Oleksandr <71518300+mralex90@users.noreply.github.com>
2021-09-28 22:39:40 +0300

    function

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
+function add() {
+    return 2 + 1
+}
+
 let headerOne = document.querySelector('.header')
 
-headerOne.innerText = 1 + 1
+headerOne.innerText = 2 + 1
 
 let headerTwo = document.querySelector('.header-two')
 
-headerTwo.innerText = 1 + 1
+headerTwo.innerText = 2 + 1
 
 let headerThree = document.querySelector('.header-three')
 
-headerThree.innerText = 1 + 1
+headerThree.innerText = 2 + 1
 
 let headerFour = document.querySelector('.header-four')
 
-headerFour.innerText = 1 + 1
+headerFour.innerText = 2 + 1
+
+let headerFive = document.querySelector('.header-five')
+
+headerFive.innerText = 2 + 1
+
+let headerSix = document.querySelector('.header-six')
+
+headerSix.innerText = 2 + 1
