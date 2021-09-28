6009d87041c7a3d4221aa1a2520ae34b5b4aac19
Oleksandr <71518300+mralex90@users.noreply.github.com>
2021-09-28 23:36:05 +0300

    parseInt
    event

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 let myButton = document.querySelector('.my-button')
 let myInput = document.querySelector('.my-input')
 let myHeader = document.querySelector('.header')
-
+let mySecondInput = document.querySelector('.input-two')
  
 
 myButton.addEventListener('click', function(){
 
-    let inputValue = myInput.value
+    let inputValue = parseInt(myInput.value)
 
-    myHeader.innerText = `Привет ${inputValue}`
+    let secondInputValue = parseInt(mySecondInput.value)
+
+    myHeader.innerText = inputValue + secondInputValue
+
+    //myHeader.innerText = `${secondInputValue} ${inputValue}`
 })
