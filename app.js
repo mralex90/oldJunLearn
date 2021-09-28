8b21d47024e3f6d2e27d52107d915f5ed5a77a70
Oleksandr <71518300+mralex90@users.noreply.github.com>
2021-09-28 23:17:48 +0300

    1t event

--- app.js
+++ app.js
@@ -2,4 +2,8 @@ let myButton = document.querySelector('.my-button')
 let myInput = document.querySelector('.my-input')
 let myHeader = document.querySelector('.header')
 
-myHeader.innerText = "Привет " + myInput.value  
+ 
+
+myButton.addEventListener('click', function(){
+    myHeader.innerText = "Привет " + myInput.value
+})
