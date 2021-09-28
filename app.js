44522816687ad8ab7b476de122ff46939662953a
Oleksandr <71518300+mralex90@users.noreply.github.com>
2021-09-28 23:47:18 +0300

    let VAT
    event

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
 let myButton = document.querySelector('.my-button')
 let myInput = document.querySelector('.my-input')
 let myHeader = document.querySelector('.header')
-let mySecondInput = document.querySelector('.input-two')
- 
 
 myButton.addEventListener('click', function(){
+    
+    let amount = parseInt(myInput.value)
+    let VAT = 0.15
 
-    let inputValue = parseInt(myInput.value)
-
-    let secondInputValue = parseInt(mySecondInput.value)
-
-    myHeader.innerText = inputValue + secondInputValue
-
-    //myHeader.innerText = `${secondInputValue} ${inputValue}`
+    myHeader.innerText = amount * VAT
 })
+
+
+// let myButton = document.querySelector('.my-button')
+// let myInput = document.querySelector('.my-input')
+// let myHeader = document.querySelector('.header')
+// let mySecondInput = document.querySelector('.input-two')
+ 
+
+// myButton.addEventListener('click', function(){
+
+//     let inputValue = parseInt(myInput.value)
+
+//     let secondInputValue = parseInt(mySecondInput.value)
+
+//     myHeader.innerText = inputValue + secondInputValue
+
+//     //myHeader.innerText = `${secondInputValue} ${inputValue}`
+// })
