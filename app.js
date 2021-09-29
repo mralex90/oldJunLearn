80910326c3d475547b8d359d6526c5a8cf17c130
Oleksandr <71518300+mralex90@users.noreply.github.com>
2021-09-30 00:14:23 +0300

    6/10  if/else start

--- app.js
+++ app.js
@@ -1,65 +1,11 @@
-let numberOne = document.querySelector('.number-one')
-let numberTwo = document.querySelector('.number-two')
+let numberOne = document.querySelector('.number')
+let button = document.querySelector('.button')
+let result = document.querySelector('.result')
 
-let plusButton = document.querySelector('.plus')
-let minusButton = document.querySelector('.minus')
-let divideButton = document.querySelector('.divide')
-let multiplyButton = document.querySelector('.multiply')
+button.addEventListener('click', function(){
 
-let clearButton = document.querySelector('.clear')
+    if(numberOne.value > 20){
+        result.innerText = numberOne.value
 
-let resultText = document.querySelector('.result')
-
-clearButton.addEventListener('click', function(){
-    resultText.innerText = ''
-    numberOne.value = ''
-    numberTwo.value = ''
-})
-
-plusButton.addEventListener('click', function(){
-
-    let numOne = parseInt(numberOne.value)
-    let numTwo = parseInt(numberTwo.value)
-    
-
-    let sum = numOne + numTwo
-
-    resultText.innerText = sum
-
-})
-
-minusButton.addEventListener('click', function(){
-
-    let numOne = parseInt(numberOne.value)
-    let numTwo = parseInt(numberTwo.value)
-    
-
-    let minus = numOne - numTwo
-
-    resultText.innerText = minus
-
-})
-
-divideButton.addEventListener('click', function(){
-
-    let numOne = parseInt(numberOne.value)
-    let numTwo = parseInt(numberTwo.value)
-    
-
-    let divide = numOne / numTwo
-
-    resultText.innerText = divide
-
-})
-
-multiplyButton.addEventListener('click', function(){
-
-    let numOne = parseInt(numberOne.value)
-    let numTwo = parseInt(numberTwo.value)
-    
-
-    let multiply = numOne * numTwo
-
-    resultText.innerText = multiply
-
+    }
 })
