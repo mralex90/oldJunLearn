06c583661a86ddc4d629978c1de513a049512388
Oleksandr <71518300+mralex90@users.noreply.github.com>
2021-09-30 00:49:27 +0300

    create <p>

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 let myInput = document.createElement('input')
 myInput.setAttribute('type', 'number')
 
+let myButton = document.createElement('button')
+myButton.innerText = 'Показать'
+
+let myParag = document.createElement('p')
+myParag.innerText = 'Здесь что-то написано'
+
+
 let body = document.querySelector('body')
 body.appendChild(myInput)
+body.appendChild(myButton)
+body.appendChild(myParag)
