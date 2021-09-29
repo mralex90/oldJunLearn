00ca89b05171cad912522605d174db5690dca15e
Oleksandr <71518300+mralex90@users.noreply.github.com>
2021-09-29 23:26:47 +0300

    error

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
-let numberOne = document.querySelector('number-one')
-let numberTwo = document.querySelector('number-two')
+let numberOne = document.querySelector('.number-one')
+let numberTwo = document.querySelector('.number-two')
 
-let plusButton = document.querySelector('plus')
-let minusButton = document.querySelector('minus')
-let divideButton = document.querySelector('divide')
-let multiplyButton = document.querySelector('multiply')
+let plusButton = document.querySelector('.plus')
+let minusButton = document.querySelector('.minus')
+let divideButton = document.querySelector('.divide')
+let multiplyButton = document.querySelector('.multiply')
+
+let resultText = document.querySelector('.result')
+
+plusButton.addEventListener('click', function(){
+
+    let numOne = parseInt(numberOne.value)
+    // let numTwo = parseInt(numberTwo.value)
+    let numTwo 
+
+    let sum = numOne + numberTwo
+
+    resultText.innerText = sum
+
+})
