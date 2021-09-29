0d8f4a368d00323d12aca5ef971ef942dca17e83
Oleksandr <71518300+mralex90@users.noreply.github.com>
2021-09-29 23:15:07 +0300

    5/10 event

--- app.js
+++ app.js
@@ -1,29 +1,7 @@
-let myButton = document.querySelector('.my-button')
-let myInput = document.querySelector('.my-input')
-let myHeader = document.querySelector('.header')
+let numberOne = document.querySelector('number-one')
+let numberTwo = document.querySelector('number-two')
 
-myButton.addEventListener('click', function(){
-    
-    let amount = parseInt(myInput.value)
-    let VAT = 0.15
-
-    myHeader.innerText = `Сумма НДС с ${amount} равна ${amount * VAT}` 
-})
-
-
-// let myButton = document.querySelector('.my-button')
-// let myInput = document.querySelector('.my-input')
-// let myHeader = document.querySelector('.header')
-// let mySecondInput = document.querySelector('.input-two')
- 
-
-// myButton.addEventListener('click', function(){
-
-//     let inputValue = parseInt(myInput.value)
-
-//     let secondInputValue = parseInt(mySecondInput.value)
-
-//     myHeader.innerText = inputValue + secondInputValue
-
-//     //myHeader.innerText = `${secondInputValue} ${inputValue}`
-// })
+let plusButton = document.querySelector('plus')
+let minusButton = document.querySelector('minus')
+let divideButton = document.querySelector('divide')
+let multiplyButton = document.querySelector('multiply')
