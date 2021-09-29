d4b48fce0d05107c9451ed177f82dd87c64beaf8
Oleksandr <71518300+mralex90@users.noreply.github.com>
2021-09-30 00:27:24 +0300

    if/else task

--- app.js
+++ app.js
@@ -4,8 +4,23 @@ let result = document.querySelector('.result')
 
 button.addEventListener('click', function(){
 
-    if(numberOne.value > 20){
-        result.innerText = numberOne.value
+    let price = 100
+    let age = numberOne.value
 
+    //Если младше 8 лет, цена = 0
+    if(age < 8){
+        price = 0
+    } 
+
+    // Если младше 20 лет, цена = цена -20%
+    if(age < 20){
+        price = price * 0.8
     }
+
+    if(age > 20){
+        price = price
+    }
+
+    result.innerText = `Цена билета равна ${price}`
+
 })
