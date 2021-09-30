6c3d4309a3ded3edcd2e0fb1574c729b46214450
Oleksandr <71518300+mralex90@users.noreply.github.com>
2021-09-30 23:32:50 +0300

    console.log

--- app.js
+++ app.js
@@ -1,19 +1,15 @@
-// let headerOne = document.createElement('h1')
-// headerOne.innerText = 1 + 1
-
-// let headerTwo = document.createElement('h1')
-// headerTwo.innerText = 1 + 1
-
-
-// body.appendChild(headerOne)
-// body.appendChild(headerTwo)
-
-
 let body = document.querySelector('body')
 
-for(let start = 1; start < 6; start = start + 1 ) {
-    let header = document.createElement('h1')
-    header.innerText = 1 + 1
+let car = 'BMW'
 
-    body.appendChild(header)
-}
+let carObj = {name: 'BMW', color: 'red'}
+let age = 'age= ' + 30
+let sum = 1 + 1
+let cityArr = ['new york', 'Tokyo', 'London']
+
+
+console.log(cityArr[0]);
+console.log(sum)
+console.log(age)
+console.log(car);
+console.log(carObj);
