df5ce0e32dd7360fd42ae78afa52a32b5c7f4628
Oleksandr <71518300+mralex90@users.noreply.github.com>
2021-09-30 23:02:19 +0300

    cycle for

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
-let headerOne = document.createElement('h1')
-headerOne.innerText = 1 + 1
+// let headerOne = document.createElement('h1')
+// headerOne.innerText = 1 + 1
 
-let headerTwo = document.createElement('h1')
-headerTwo.innerText = 1 + 1
+// let headerTwo = document.createElement('h1')
+// headerTwo.innerText = 1 + 1
+
+
+// body.appendChild(headerOne)
+// body.appendChild(headerTwo)
+
 
 let body = document.querySelector('body')
 
-body.appendChild(headerOne)
-body.appendChild(headerTwo)
+for(let start = 1; start < 6; start = start + 1 ) {
+    let header = document.createElement('h1')
+    header.innerText = 1 + 1
+
+    body.appendChild(header)
+}
